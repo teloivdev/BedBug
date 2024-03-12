814e933e4328b73a2455d1431cdd623c07a3c927
cajungirl1504 <afcsupport@afchomeclub.com>
2024-03-12 11:03:31 -0400

    my changes

---
+++ resources/views/layouts/webNav.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary nav-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="{{ asset('/images/bedBugsLogo.png') }}" width="125" height="125" alt="Bed Bugs insurance logo"></a>
+            <button class="navbar-toggler navbar-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">About</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">About Us</a></li>
+                        <li><a class="dropdown-item" href="#">Property Owner</a></li>
+                        <li><a class="dropdown-item" href="#">Property Manager</a></li>
+                        <li><a class="dropdown-item" href="#">Bed Bug Information</a></li>
+                    </ul>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Property Manager Register</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Property Register</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Documents</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Claims</a>
+                    </li>
+                </ul>
+            </div>
+    </div>
+</nav>
--- resources/views/website/home/index.blade.php
+++ resources/views/website/home/index.blade.php
@@ -1,6 +1,126 @@
 @include('layouts.main')
+@include('layouts.webNav')
 <body>
-    <div class="container">
+    <div class="container-fluid" id="homeHeader">
+        <div class="row">
+            <div class="col-12"> 
+                <div class="container">
+                    <div class="row">
+                        <div class="offset-md-2 col-md-8 col-12">
+                            <h1 class="display-2 fw-bold text-white pt-3 pb-3">Protect Your Property</h1>
+                            <h2 class="fw-bold text-white fs-3 pt-3 pb-3">Save Money and Get Peace of Mind with Bed Bug Insurance</h2>
+                            <p class="fs-5 text-white">A bed bug infestation is a threat to any property owner, and we are providing a solution that gives all property managers peace of mind. From a small apartment to a multi-floor townhouse, we will protect your property with our Bed Bug Insurance. This insurance provides coverage for lost revenue, provides the necessary treatment to resolve the infestation, and provides peace of mind for you as you know your property is protected. Find out how we can get you covered now.</p>
+                        </div>
+                    </div>
+                    <div class="row d-flex justify-content-center pt-5 pb-4">
+                        <div class="col-12 text-center">
+                            <div class="d-grid gap-2 col-2 mx-auto">
+                            <button class="btn btn-outline-primary btn-lg bg-primary text-white" role="button" type="button">Get Covered</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container pt-5 pb-5">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="fw-bold fs-1 text-center">Don't Wait. Get Covered.</h2>
+            </div>
+        </div>
+        <div class="row pt-5">
+            <div class="col-md-6 col-12">
+                <ul class="list-group">
+                    <li class="list-group-item pt-2 pb-2">
+                        <div class="row d-flex align-items-top">
+                            <div class="col-md-1 col-1">
+                                <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/1B5BD1/bug--v1.png" alt="bug--v1"/>
+                            </div>
+                            <div class="col-md-11 col-11 fs-5">Bed Bugs Affect One in Five Americans</div>
+                        </div>
+                    </li>
+                    <li class="list-group-item pt-2 pb-2">
+                        <div class="row d-flex align-items-top">
+                            <div class="col-md-1 col-1">
+                                <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/1B5BD1/bug--v1.png" alt="bug--v1"/>
+                            </div>
+                            <div class="col-md-11 col-11 fs-5">Multifamily Properties Rank Second In Yearly Bed Bug Infestations</div>
+                        </div>
+                    </li>
+                    <li class="list-group-item pt-2 pb-2">
+                        <div class="row d-flex align-items-top">
+                            <div class="col-md-1 col-1">
+                                <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/1B5BD1/bug--v1.png" alt="bug--v1"/>
+                            </div>
+                            <div class="col-md-11 col-11 fs-5">Without Coverage, Properties Incur Thousands of Dollars in Damages</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>          
+            <div class="col-md-6 col-12">
+                <ul class="list-group">
+                    <li class="list-group-item pt-2 pb-2">
+                        <div class="row d-flex align-items-top">
+                            <div class="col-md-1 col-1">
+                                <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/1B5BD1/bug--v1.png" alt="bug--v1"/>
+                            </div>
+                            <div class="col-md-11 col-11 fs-5">In Partnership with Collective Underwriters, Inc, We Have the Solution</div>
+                        </div>
+                    </li>
+                    <li class="list-group-item pt-2 pb-2">
+                        <div class="row d-flex align-items-top">
+                            <div class="col-md-1 col-1">
+                                <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/1B5BD1/bug--v1.png" alt="bug--v1"/>
+                            </div>
+                            <div class="col-md-11 col-11 fs-5">Two Forms, Affordable Annual Payments, and Protection Against Infestations</div>
+                        </div>
+                    </li>
+                    <li class="list-group-item pt-2 pb-2">
+                        <div class="row d-flex align-items-top">
+                            <div class="col-md-1 col-1">
+                                <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/1B5BD1/bug--v1.png" alt="bug--v1"/>
+                            </div>
+                            <div class="col-md-11 col-11 fs-5">Remove The Fear of Bed Bugs and Rest Easy with Bed Bug Insurance</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>          
+        </div>
+    </div>
+    <div class="container pt-5 pb-5">
+        <div class="row">
+            <div class="col-3">
+                <p>test</p>
+            </div>
+            <div class="col-3">
+                <p>test</p>
+            </div>
+            <div class="col-3">
+                <p>test</p>
+            </div>
+            <div class="col-3">
+                <p>test</p>
+            </div>
+        </div>
+    </div>
+    <div class="container pt-5 pb-5">
+        <div class="row">
+            <div class="col-3">
+                <p>test</p>
+            </div>
+            <div class="col-3">
+                <p>test</p>
+            </div>
+            <div class="col-3">
+                <p>test</p>
+            </div>
+            <div class="col-3">
+                <p>test</p>
+            </div>
+        </div>
+    </div>
+    <div class="container pt-5 pb-5">
         <div class="row">
             <div class="col-3">
                 <p>test</p>
